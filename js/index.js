125d5202baabdf11e3f3fd10c821c2b73c9fa4fe
Warren Rawlings <rawlings.warren@gmail.com>
2019-08-17 17:21:20 -0400

    following the tutorial at 50min

--- js/index.js
+++ js/index.js
@@ -1,1 +1,77 @@
-// make some Javascript- this will help understand components
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav')
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+//console.log(track)
+
+//arrange size next to one another
+// slides[0].style.left = slideWidth * 0 + 'px';
+// slides[1].style.left = slideWidth * 1 + 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSlidePosition);
+//
+// slides.forEach((slide, index) => {
+//     slide.getElementsByClassName.left = slideWidth * index + 'px';
+// })
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+//when I click left move slides to the left
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+
+    moveToSlide(track, currentSlide, prevSlide)
+    updateDots(currentDot, prevDot);
+})
+
+
+//when I click right - move to the right
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    //const amountToMove = nextSlide.style.left;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+})
+
+//when I click navigation dot indicators- move to that slide
+dotsNav.addEventListener('click', e => {
+    //what indicator was clicked on?
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot)
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+
+    updateDots(currentDot, targetDot);
+})
