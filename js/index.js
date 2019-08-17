e6a44fdd50a5dc7ebf608f4edd9f02781b06c6d9
Warren Rawlings <rawlings.warren@gmail.com>
2019-08-17 17:48:35 -0400

    completed tutorial- took a long time!

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-//console.log(track)
 
 //arrange size next to one another
 // slides[0].style.left = slideWidth * 0 + 'px';
@@ -35,15 +34,32 @@ const updateDots = (currentDot, targetDot) => {
     targetDot.classList.add('current-slide');
 }
 
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
+
 //when I click left move slides to the left
 prevButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current-slide');
     const prevSlide = currentSlide.previousElementSibling;
     const currentDot = dotsNav.querySelector('.current-slide');
     const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
 
     moveToSlide(track, currentSlide, prevSlide)
     updateDots(currentDot, prevDot);
+
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
 })
 
 
@@ -54,9 +70,12 @@ nextButton.addEventListener('click', e => {
     //const amountToMove = nextSlide.style.left;
     const currentDot = dotsNav.querySelector('.current-slide');
     const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
 
     moveToSlide(track, currentSlide, nextSlide);
     updateDots(currentDot, nextDot);
+
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
 })
 
 //when I click navigation dot indicators- move to that slide
@@ -74,4 +93,6 @@ dotsNav.addEventListener('click', e => {
     moveToSlide(track, currentSlide, targetSlide);
 
     updateDots(currentDot, targetDot);
+
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
 })
